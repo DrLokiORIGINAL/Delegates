855a1b9500c985ea151ed84de2f2f3394a718591
wsr11 <wsr11@college.edu>
2022-04-07 17:16:06 +0600

    Добавьте файлы проекта.

---
+++ Delegates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate void MyDelegate();
+
+    class SomeClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void HelloMethod(int a, int b)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate mD = new MyDelegate(SomeClass.Method);
+
+            mD.Invoke();
+
+            mD();
+
+            Console.ReadKey();
+        }
+    }
+}
